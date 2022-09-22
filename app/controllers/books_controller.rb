8c7cae22798dda1200156cd41f80c1800f28a01b
EC2 Default User <ec2-user@ip-172-31-2-47.ap-northeast-1.compute.internal>
2022-09-22 19:05:22 +0000

    add navbar

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,29 @@
+#Bookers2
 class BooksController < ApplicationController
   def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to books_path(@book.id)
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+    @book.update
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,21 @@
 class UsersController < ApplicationController
   def index
+    @user = User.find(params[:id])
+    @users = User.all
   end
 
   def show
+    @user = User.find(current_user_params[:id])
+    @book = Book.new
+    @books = current_user.books.all
   end
 
   def edit
+    @user = User.find(current_user_params:id)
   end
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
 end
